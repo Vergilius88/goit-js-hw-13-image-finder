c88495fbf959a014c435cee112fd5695985e1574
Nikolaienko Ihor <nikolasha8876@gmail.com>
2020-10-24 16:19:59 +0300

    naming

--- src/js/apdate-markup.js
+++ src/js/apdate-markup.js
@@ -2,8 +2,8 @@ import imagesTemplateItem from '../templates/tamplate-item.hbs';
 import refs from './refs.js';
 import serviceScrollPosition from './update-position.js';
 export default {
-  updateMarkup(item) {
-    refs.galleryRef.insertAdjacentHTML('beforeend', imagesTemplateItem(item));
+  updateMarkup(items) {
+    refs.galleryRef.insertAdjacentHTML('beforeend', imagesTemplateItem(items));
     refs.buttonRef.classList.remove('hiden');
     serviceScrollPosition.updatePosition();
   },
